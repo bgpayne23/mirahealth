0b04b4e1a25bfe9bbe0a17ef85f8ccebfedd6fa9
Tyler Ferguson <tyler.ferguson@ibm.com>
2023-07-27 21:06:22 -0500

    cleaned up some code w prettier/spacing

--- mira-health-ui/pages/alerts/PredictionsPage.tsx
+++ mira-health-ui/pages/alerts/PredictionsPage.tsx
@@ -1,26 +1,26 @@
 // NOT USING
 
-import React from 'react';
-import PregnancyPredictionsDisplay from '@/components/PregnancyPredictionsDisplay';
+import React from "react";
+import PregnancyPredictionsDisplay from "@/components/PregnancyPredictionsDisplay";
 
 // Define the PredictionData interface here
 interface PredictionData {
     prediction: number;
     score: number;
-  }
+}
 
 const PredictionsPage: React.FC = () => {
-  // Assume you have received the prediction data from the server
-  const predictionData: PredictionData = {
-    prediction: 0,
-    score: 0.12345678,
-  };
+    // Assume you have received the prediction data from the server
+    const predictionData: PredictionData = {
+        prediction: 0,
+        score: 0.12345678,
+    };
 
-  return (
-    <div className="container mx-auto mt-8">
-      <PregnancyPredictionsDisplay data={predictionData} />
-    </div>
-  );
+    return (
+        <div className="container mx-auto mt-8">
+            <PregnancyPredictionsDisplay data={predictionData} />
+        </div>
+    );
 };
 
 export default PredictionsPage;
--- mira-health-ui/src/components/PregnancyPredictionsDisplay.tsx
+++ mira-health-ui/src/components/PregnancyPredictionsDisplay.tsx
@@ -1,29 +1,31 @@
-import React from 'react';
+import React from "react";
 
 type PredictionData = {
-  prediction: number;
-  score: number;
+    prediction: number;
+    score: number;
 };
 
 interface PregnancyPredictionsProps {
-  data: PredictionData;
+    data: PredictionData;
 }
-// todo: update styling and presentation! 
+// todo: update styling and presentation!
 // todo: pass data from response to here
-const PregnancyPredictionsDisplay: React.FC<PregnancyPredictionsProps> = ({ data }) => {
-  return (
-    <div className="bg-white p-4 rounded-lg shadow-md">
-      <h2 className="text-2xl font-bold mb-4">Pregnancy Predictions</h2>
-      <div>
-        <p className="text-lg font-bold">Prediction:</p>
-        <p className="text-xl">{data.prediction}</p>
-      </div>
-      <div>
-        <p className="text-lg font-bold">Score:</p>
-        <p className="text-xl">{data.score}</p>
-      </div>
-    </div>
-  );
+const PregnancyPredictionsDisplay: React.FC<PregnancyPredictionsProps> = ({
+    data,
+}) => {
+    return (
+        <div className="bg-white p-4 rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold mb-4">Pregnancy Predictions</h2>
+            <div>
+                <p className="text-lg font-bold">Prediction:</p>
+                <p className="text-xl">{data.prediction}</p>
+            </div>
+            <div>
+                <p className="text-lg font-bold">Score:</p>
+                <p className="text-xl">{data.score}</p>
+            </div>
+        </div>
+    );
 };
 
 export default PregnancyPredictionsDisplay;
