038d3db3d79544e504dd569cadffac8cee366a77
Emily Bordelon <Emily.Bordelon@ibm.com>
2023-08-02 12:19:47 -0500

    Add circular progress bar and bar charts for Health Factors and Socioeconomic Factors to PregnancyPredictionsDisplay

--- mira-health-ui/src/components/PregnancyPredictionsDisplay.tsx
+++ mira-health-ui/src/components/PregnancyPredictionsDisplay.tsx
@@ -2,11 +2,15 @@ import React, { useContext, useEffect, useState } from "react";
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import FormDataContext from "./FormDataContext";
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import Bar from './ui/bar';
 
 type PredictionData = {
     prediction: number;
     score: number;
 };
+
 
 // You might not need this prop anymore
 interface PregnancyPredictionsProps {
@@ -17,13 +21,9 @@ const PregnancyPredictionsDisplay: React.FC<PregnancyPredictionsProps> = ({ }) =
     const router = useRouter();
     const [apiResponse, setApiResponse] = useState<PredictionData>({
         prediction: 0,
-        score: 0,
+        score: 0
     });
     const { formData } = useContext(FormDataContext);
-
-
-
-
 
     useEffect(() => {
         const requiredFields = [
@@ -66,20 +66,132 @@ const PregnancyPredictionsDisplay: React.FC<PregnancyPredictionsProps> = ({ }) =
         }
     }, [formData]);
 
+    const percentage = parseFloat((Number(apiResponse.score) * 100).toFixed(2));
+    const pathColor = percentage >= .5 ? '#ef4444' : '#34d399';
+    const highRiskMessage = "Patient has high likelihood of HDP";
+    const lowRiskMessage = "Patient has low likelihood of HDP";
+    //const mediumRiskMessage = "Patient has medium likelihood of HDP";
+    const messageToDisplay = percentage >= .5 ? highRiskMessage : lowRiskMessage;
+
+    const value = 75;
+    const maxValue = 100;
+
+
     return (
-            <div className="flex justify-center items-start h-screen mt-48">
-                <div className={`p-24 rounded-full shadow-md w-96 flex flex-col items-center ${apiResponse.score < 0.5 ? 'bg-green-300' : 'bg-red-300'}`}>
-                    <div className="text-center py-4">
-                        <p className="text-3xl font-bold">Prediction:</p>
-                        <p className="text-4xl">{apiResponse.prediction}</p>
-                    </div>
-                    <div className="text-center">
-                        <p className="text-3xl font-bold">Score:</p>
-                        <p className="text-4xl">{(Number(apiResponse.score) * 100).toFixed(2)} %</p>
+
+        <div className="flex flex-col justify-center items-center h-screen mt-16 w-screen">
+            <div className="flex justify-center" style={{ width: 300, height: 300 }}>
+                <CircularProgressbar
+                    value={percentage}
+                    text={`${(Number(apiResponse.score) * 100).toFixed(2)}%`}
+                    styles={buildStyles({
+                        strokeLinecap: 'round',
+                        textSize: '18px',
+                        pathTransitionDuration: 0.5,
+                        pathColor: `${pathColor}`,
+                        textColor: '#1c1917',
+                        trailColor: '#d6d6d6',
+                        backgroundColor: '#3e98c7',
+                    })}
+                    className=''
+                />
+            </div>
+
+            <div className="flex justify-center items-start mt-8">
+                <div className={`p-4 mb-4 rounded-md shadow-md flex flex-col items-center ${apiResponse.score < 0.5 ? 'bg-green-300' : 'bg-red-300'}`}>
+                    <div className="text-center py-1">
+                        <p className="text-3xl font-bold">Alert</p>
+                        <p className="text-2xl">{messageToDisplay}</p>
                     </div>
                 </div>
             </div>
-        
+
+            <div className="flex justify-center items-start mt-1 w-full">
+                <div className={`mt-4 shadow-md w-1/4 flex flex-col bg-white-100 border-2`}>
+                    <div className="text-center py-1">
+                        <div className="text-xl font-bold">Health Factors</div>
+                        <Bar value={value} max={maxValue} color="purple" height={30} factorName={"BP"} />
+                        <Bar value={value} max={maxValue} color="purple" height={30} factorName={"BS"} />
+                        <Bar value={value} max={maxValue} color="purple" height={30} factorName={"Age"} />
+                    </div>
+                </div>
+                <div className={`mt-4 shadow-md w-1/4 flex flex-col bg-white-100 border-2`}>
+                    <div className="text-center py-1">
+                        <div className="text-xl font-bold">Socioeconomic Factors</div>
+                        <Bar value={value} max={maxValue} color="purple" height={30} factorName={"Location"} />
+                        <Bar value={value} max={maxValue} color="purple" height={30} factorName={"OBGYN"} />
+                        <Bar value={value} max={maxValue} color="purple" height={30} factorName={"Payer"} />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        // <div className="flex flex-col justify-center items-start h-screen mt-16">
+        //     <div className="flex">
+        //         <div className="flex justify-center"
+        //             style={{ width: 300, height: 300 }}>
+        //             <CircularProgressbar
+        //                 value={percentage}
+        //                 text={`${(Number(apiResponse.score) * 100).toFixed(2)}%`}
+        //                 styles={buildStyles({
+        //                     // Rotation of path and trail, in number of turns (0-1)
+        //                     // rotation: 0.25,
+
+        //                     // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+        //                     strokeLinecap: 'round',
+
+        //                     // Text size
+        //                     textSize: '18px',
+
+        //                     // How long animation takes to go from one percentage to another, in seconds
+        //                     pathTransitionDuration: 0.5,
+
+        //                     // Can specify path transition in more detail, or remove it entirely
+        //                     // pathTransition: 'none',
+
+        //                     // Colors
+        //                     pathColor: `${pathColor}`,
+        //                     textColor: '#1c1917',
+        //                     trailColor: '#d6d6d6',
+        //                     backgroundColor: '#3e98c7',
+        //                 })}
+        //                 className='' />
+        //         </div>
+
+        //     </div>
+
+        //     <div className="flex justify-center items-start h-screen mt-8">
+        //         <div className={`p-1 rounded-md shadow-md w-96 flex flex-col items-center ${apiResponse.score < 0.5 ? 'bg-purple-300' : 'bg-red-300'}`}>
+        //             <div className="text-center py-1">
+        //                 <p className="text-3xl font-bold">Alert:</p>
+        //                 <p className="text-4xl">{messageToDisplay}</p>
+        //             </div>
+        //         </div>
+        //     </div>
+
+        //     <div className="flex justify-center items-start h-screen mt-1">
+        //         <div className={`p-1 rounded-md shadow-md w-96 flex flex-col items-center ${apiResponse.score < 0.5 ? 'bg-purple-300' : 'bg-red-300'}`}>
+        //             <div className="text-center py-1">
+        //                 <Bar value={value} max={maxValue} color="purple" width={300} height={30} factorName={"BP"} />
+        //                 <Bar value={value} max={maxValue} color="purple" width={300} height={30} factorName={"BS"} />
+        //                 <Bar value={value} max={maxValue} color="purple" width={300} height={30} factorName={"Age"} />
+        //             </div>
+        //         </div>
+        //         <div className={`p-1 rounded-md shadow-md w-96 flex flex-col items-center ${apiResponse.score < 0.5 ? 'bg-purple-300' : 'bg-red-300'}`}>
+        //             <div className="text-center py-1">
+        //                 <div className="text-center py-1">
+        //                     <Bar value={value} max={maxValue} color="purple" width={300} height={30} factorName={"Location"} />
+        //                     <Bar value={value} max={maxValue} color="purple" width={300} height={30} factorName={"OBGYN"} />
+        //                     <Bar value={value} max={maxValue} color="purple" width={300} height={30} factorName={"Payer"} />
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+
+        // </div>
+
+
 
     );
 };
